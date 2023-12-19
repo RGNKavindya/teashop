28621e3f7a8ba9c08b2604d03400ba87cab893d1
OGPasindu <ogpmadhuwantha678@gmail.com>
2023-12-20 00:43:43 +0530

    Added node files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tailwind css Test</title>
+
+    <!-- custom style.css -->
+    <link rel="stylesheet" href="style.css" />
+
+    <!-- tailwind -->
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+
+<body>
+
+    
+
+</body>
+
+</html>
